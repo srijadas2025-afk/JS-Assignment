ba0a718961affe3db53685b2288040f7826c5e7a
SRIJA DAS <srija.das09007@gmail.com>
2026-04-14 15:25:31 +0000

    added question 2

--- question2.js
+++ question2.js
@@ -1,1 +1,27 @@
 
+function runQ2() {
+    var N    = parseInt(prompt("Enter N:"));
+    var seed = parseInt(prompt("Enter seed:"));
+
+    var current = N;
+
+    for (var i = 0; i < 3; i++) {
+        if (current % 2 === 0) {
+            current = current / 2 + seed;
+        } else {
+            current = current * 3 - seed;
+        }
+    }
+
+    var isThreeDigit = (current >= 100 && current <= 999);
+
+    var s = "" + current;
+    var middleDigit = parseInt(s.charAt(1));
+    var middleMatches = (middleDigit === seed);
+
+    if (isThreeDigit && middleMatches) {
+        alert("YES, " + current);
+    } else {
+        alert("NO, " + current);
+    }
+}
