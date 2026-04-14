584752f9caa46c2df5390b4c970be66ffecd53b2
SRIJA DAS <srija.das09007@gmail.com>
2026-04-14 15:40:08 +0000

    did question 4

--- question4.js
+++ question4.js
@@ -1,1 +1,38 @@
 
+function runQ4() 
+{
+    var base        = parseInt(prompt("Enter base:"));
+    var distance    = parseInt(prompt("Enter distance:"));
+    var minutesLate = parseInt(prompt("Enter minutesLate:"));
+    var seed        = parseInt(prompt("Enter seed:"));
+
+    
+    var fare = base + 7 * distance;
+
+    
+    if (minutesLate > 15) 
+    {
+        fare = fare + 20;
+    }
+
+    
+    if (distance > 10) 
+    {
+        fare = fare + Math.floor(0.10 * fare);
+    }
+
+    
+    if (seed % 2 !== 0) 
+    {
+        fare = fare - seed;   
+    } 
+    else 
+    {
+        fare = fare + seed;   
+    }
+
+    
+    fare = Math.ceil(fare / 5) * 5;
+
+    alert("Final fare: "+fare);
+}
